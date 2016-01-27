815f95b5bd43b2508e82f51424f1111588797ab3
brunoric <brunoric@gmail.com>
2016-01-27 18:35:58 -0200

    Using parent getter for lastHeaders.
    The property lastHeaders is private on the parent class. Removed
    direct access to use the parent getter.

--- src/CurlRemoteFilesystem.php
+++ src/CurlRemoteFilesystem.php
@@ -173,7 +173,7 @@ class CurlRemoteFilesystem extends Util\RemoteFilesystem
      */
     public function getLastHeaders()
     {
-        return $this->lastHeaders;
+        return parent::getLastHeaders();
     }
 
     /**
