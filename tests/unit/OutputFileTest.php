d5b36373959dc6f5729821d1894b17cb2b5b094f
Hiraku NAKANO <hiraku@tojiru.net>
2016-01-31 00:40:56 +0900

    Fixed OutputFile test

--- tests/unit/OutputFileTest.php
+++ tests/unit/OutputFileTest.php
@@ -3,6 +3,11 @@ namespace Hirak\Prestissimo;
 
 class OutputFileTest extends \PHPUnit_Framework_TestCase
 {
+    protected function setUp()
+    {
+        chmod('tests/workspace/cannotWritable', 0555);
+    }
+
     public function testNewAndAutoClean()
     {
         $fileName = 'tests/workspace/test/example.txt';
