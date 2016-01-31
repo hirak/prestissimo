4953af844bae0a198d2c2f6d1cd2eac629291843
Hiraku NAKANO <hiraku@tojiru.net>
2016-01-31 16:15:22 +0900

    Added simple http server for test

---
+++ tests/workspace/sampleapi.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * test http server
+ *
+ * php http.php
+ *
+ * exit:  http://localhost:1337/?exit=1
+ * delay:  http://localhost:1337/?wait=1 (seconds)
+ */
+set_time_limit(0);
+ob_implicit_flush();
+
+$response =<<<_HTTP_
+HTTP/1.1 200 OK\r
+Content-Type: text/plain\r
+Content-Length: 1\r
+\r
+
+_HTTP_;
+
+$context['socket']['backlog'] = 128;
+$server = stream_socket_server(
+    'tcp://0.0.0.0:1337',
+    $errno,
+    $errmsg,
+    STREAM_SERVER_LISTEN | STREAM_SERVER_BIND,
+    stream_context_create($context)
+);
+stream_set_blocking($server, 0);
+stream_set_timeout($server, 0);
+
+$readOrigin = array();
+$readOrigin[(int)$server] = $server;
+$writeOrigin = array();
+$except = null;
+
+$waiting = array();
+
+LOOP: {
+    $read = $readOrigin;
+    $write = $writeOrigin;
+
+    stream_select($read, $write, $except, 1); //block
+
+    if (PHP_VERSION_ID < 50400) {
+        $readfix = array();
+        foreach ($read as $stream) {
+            $readfix[(int)$stream] = $stream;
+        }
+        $read = $readfix;
+    }
+
+    if (isset($read[(int)$server])) {
+        $connection = stream_socket_accept($server, 30, $peername);
+        stream_set_blocking($connection, 0);
+        $readOrigin[(int)$connection] = $connection;
+        unset($read[(int)$server]);
+    }
+
+    $now = microtime(true);
+    foreach ($waiting as $waitingStream) {
+        list($stream, $until, $messageBody) = $waitingStream;
+        if ($now > $until) {
+            $index = (int)$stream;
+            fwrite($stream, $response . $messageBody);
+            fclose($stream);
+            unset($waiting[$index]);
+            unset($readOrigin[$index]);
+        }
+    }
+
+    foreach ($read as $stream) {
+        $requestLine = stream_get_line($stream, 4096, "\r\n");
+
+        list(, $path,) = explode(' ', $requestLine);
+        parse_str(parse_url($path, PHP_URL_QUERY), $query);
+        if (isset($query['exit']) && $query['exit'] === '1') exit;
+
+        if (isset($query['wait']) && $query['wait'] > 0) {
+            $waiting[(int)$stream] = array(
+                $stream,
+                microtime(true) + (int)$query['wait'],
+                (int)$query['wait'],
+            );
+        } else {
+            fwrite($stream, $response . '0');
+            fclose($stream);
+        }
+        unset($readOrigin[(int)$stream]);
+    }
+
+    goto LOOP;
+}
