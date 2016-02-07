650cf126b06d88842983e84f9c6e869788d24c6b
Hiraku NAKANO <hiraku@tojiru.net>
2016-02-07 11:03:01 +0900

    Updated test for JoinPoint

--- tests/unit/Aspects/JoinPointTest.php
+++ tests/unit/Aspects/JoinPointTest.php
@@ -12,8 +12,46 @@ class JoinPointTest extends \PHPUnit_Framework_TestCase
             'https://example.com/',
             new IO\NullIO
         );
-
         $joinPoint = new JoinPoint('pre-download', $req);
         self::assertInstanceOf('Hirak\Prestissimo\Aspects\JoinPoint', $joinPoint);
+
+        self::assertSame($req, $joinPoint->refRequest());
+    }
+
+    public function testSetRequest()
+    {
+        $req = new HttpGetRequest(
+            'example.com',
+            'https://example.com/',
+            new IO\NullIO
+        );
+        $joinPoint = new JoinPoint('pre-download', $req);
+
+        $req2 = new HttpGetRequest(
+            'example.com',
+            'https://example.com/',
+            new IO\NullIO
+        );
+        $joinPoint->setRequest($req2);
+        self::assertSame($req2, $joinPoint->refRequest());
+    }
+
+    public function testAttachAndDetach()
+    {
+        $observer = $this->prophesize('SplObserver');
+
+        $joinPoint = new JoinPoint('pre-download', new HttpGetRequest(
+            'example.com',
+            'https://example.com/',
+            new IO\NullIO
+        ));
+        $joinPoint->attach($observer->reveal());
+
+        $observer->update($joinPoint)->shouldBeCalled();
+        $joinPoint->notify();
+
+        $joinPoint->detach($observer->reveal());
+        $observer->update($joinPoint)->shouldNotBeCalled();
+        $joinPoint->notify();
     }
 }
