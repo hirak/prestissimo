52ec6cf49ad7789c10d7be982ef3d0f18ccaee73
Hiraku NAKANO <hiraku@tojiru.net>
2016-06-02 09:28:14 +0900

    resurrection CurlRemoteFilesystem

---
+++ src/CurlRemoteFilesystem.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * hirak/prestissimo
+ * @author Hiraku NAKANO
+ * @license MIT https://github.com/hirak/prestissimo
+ */
+namespace Hirak\Prestissimo;
+
+use Composer\Config;
+use Composer\IO;
+use Composer\Downloader;
+use Composer\Util;
+
+class CurlRemoteFilesystem extends Util\RemoteFilesystem
+{
+    protected $io, $config, $options, $disableTls;
+
+    private $req;
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct(IO\IOInterface $io, Config $config = null, array $options = array(), $disableTls = false)
+    {
+        $this->io = $io;
+        $this->config = $config;
+        $this->options = $options;
+        $this->disableTls = $disableTls;
+        parent::__construct($io, $config, $options, $disableTls);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getContents($fileUrl, $progress = true, $options = array())
+    {
+        try {
+            $this->req = new FetchRequest($fileUrl, $progress, $options);
+            $res = $this->req->fetch();
+        } catch (\Exception $e) {
+            $this->io->writeError((string)$e);
+        }
+        if ($res) {
+            return $res;
+        } else {
+            return parent::getContents($originUrl, $fileUrl, $options, $progress);
+        }
+    }
+
+    public function getLastHeaders()
+    {
+        if ($this->req && ($headers = $this->req->getLastHeaders())) {
+            return $headers;
+        } else {
+            return parent::getLastHeaders();
+        }
+    }
+
+    public function __debugInfo()
+    {
+        return array();
+    }
+}
