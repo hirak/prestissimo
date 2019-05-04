2fa7f810e77b66063fe03b9bdbc755002864f66a
Hiraku NAKANO <hiraku@tojiru.net>
2019-05-04 14:04:31 +0900

    update gulpjs to v4.x

--- gulpfile.js
+++ gulpfile.js
@@ -5,63 +5,71 @@
  * 2. $ npm install
  * 3. $ npm start
  */
-var gulp = require('gulp');
-var exec = require('child_process').exec;
-var bs = require('browser-sync').create();
+const { parallel, watch } = require('gulp');
+const { exec } = require('child_process');
+const bs = require('browser-sync').create();
 
-gulp.task('default', ['test', 'inspect']);
+exports.default = parallel(test, inspect)
 
 function test(done) {
-    var p = exec('composer fasttest');
-    p.stdout.pipe(process.stdout);
-    p.stderr.pipe(process.stderr);
-    p.on('exit', done);
-}
+  var p = exec('composer fasttest');
+  p.stdout.pipe(process.stdout);
+  p.stderr.pipe(process.stderr);
+  p.on('exit', done);
+};
+
+exports.test = test;
 
 function inspect(done) {
-    var r = 3;
-    function wait() {
-        --r || done();
-    }
-    exec('composer doc', wait);
-    exec('composer metrics', wait);
-    var lint = exec('composer lint'); 
-    lint.stdout.pipe(process.stdout);
-    lint.stderr.pipe(process.stderr);
-    lint.on('exit', wait);
-}
+  var r = 3;
+  function wait() {
+    --r || done();
+  }
+  exec('composer doc', wait);
+  exec('composer metrics', wait);
+  var lint = exec('composer lint'); 
+  lint.stdout.pipe(process.stdout);
+  lint.stderr.pipe(process.stderr);
+  lint.on('exit', wait);
+};
+
+exports.inspect = inspect;
 
 function lint(target, done) {
-    var p = exec('composer lint -- ' + target);
-    p.stdout.pipe(process.stdout);
-    p.stderr.pipe(process.stderr);
-    p.on('exit', done);
+  var p = exec('composer lint -- ' + target);
+  p.stdout.pipe(process.stdout);
+  p.stderr.pipe(process.stderr);
+  p.on('exit', done);
 }
 
-gulp.task('test', test);
-gulp.task('inspect', inspect);
+exports.start = function start(done) {
+  bs.init({
+    server: {
+      baseDir: "artifacts/"
+    },
+    ghostMode: false
+  });
 
-gulp.task('start', function(){
-    bs.init({
-        server: {
-            baseDir: "artifacts/"
-        },
-        ghostMode: false
+  const watcher = watch(['src/**/*.php', 'tests/**/*Test.php']);
+
+  watcher.on('change', function(path, stats) {
+    console.log(`File ${path} was changed`);
+    lint(path, function(){
+      test(bs.reload);
     });
+  });
 
-    gulp.watch(['src/**/*.php', 'tests/**/*Test.php'], {}, function(ev){
-        switch (ev.type) {
-            case "added":
-            case "changed":
-                lint(ev.path, function(){
-                    test(bs.reload);
-                });
-                break;
-            default:
-                inspect(function(){
-                    test(bs.reload);
-                });
-        }
+  watcher.on('add', function(path, stats) {
+    console.log(`File ${path} was added`);
+    lint(path, function(){
+      test(bs.reload);
     });
-});
+  });
 
+  watcher.on('unlink', function(path, stats) {
+    console.log(`File ${path} was unlinked`);
+    inspect(function(){
+      test(bs.reload);
+    });
+  });
+};
